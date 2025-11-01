fb0a82f400504dab7a80c219f105af155dc17016
leeboyeon17 <2023110695@dgu.ac.kr>
2025-11-02 03:11:17 +0900

    chore: normalize line endings (LF)

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,13 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // 페이지 import
 import Home from "../pages/Home";
+import DesignSystem from "../pages/DesignSystem";
 
 // 페이지 라우팅
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
+        {/*임시*/}
         <Route path="/" element={<Home />} />
+        <Route path="/designsystem" element={<DesignSystem />} />
       </Routes>
     </BrowserRouter>
   );
