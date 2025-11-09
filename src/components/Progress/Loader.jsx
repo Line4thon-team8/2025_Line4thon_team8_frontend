50d66ce142c18c255c1330832c865a5962ab3d57
tpdus <tpdus6401@naver.com>
2025-11-09 14:01:01 +0900

    feat/#8: Progress UI구현

---
+++ src/components/Progress/Loader.jsx
@@ -0,0 +1,38 @@
+import {useState} from 'react';
+import { ClipLoader } from 'react-spinners';
+import styled from "styled-components";
+
+const Loader = () => {
+    const [loading, setLoading] = useState(true);
+
+    const override = {
+        borderWidth: "6px", //두께
+        borderColor: "#ffffff #226CFF #226CFF #ffffff"
+    };
+
+    return (
+        <LoaderWrap>
+            <ClipLoader
+            color="#226CFF" //메인 컬러
+            size={44} //지름
+            speedMultiplier={0.5} //속도
+            cssOverride={override}
+            />
+        </LoaderWrap>
+        
+    )
+}
+
+export default Loader;
+
+const LoaderWrap = styled.div`
+    background-color: #226CFF75;
+    width: 90px;
+    height: 90px;
+    padding: 22px 22px 22px 23px;
+    border-radius: 20px;
+
+    display: flex;
+    align-items: center;
+    flex-direction: center;
+`;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -5,6 +5,8 @@ import Layout from "../pages/Layout";
 // Pages
 import Home from "../pages/Home";
 import DesignSystem from "../pages/DesignSystem";
+import Progress from "../pages/Progress/Progress";
+// import Summation from "../pages/SummationPage/Summation";
 
 
 export default function Router() {
@@ -14,6 +16,8 @@ export default function Router() {
         
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/progress" element={<Progress />} />
+          {/* <Route path="/summation" element={<Summation />} /> */}
         </Route>
 
         <Route path="/designsystem" element={<DesignSystem />} />
